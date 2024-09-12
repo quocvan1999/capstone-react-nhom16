c0a0d100149e186717b3a66b216245bf13c28ebd
Quocvan <lyquocvan1999@gmail.com>
2024-09-12 20:45:00 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 });
-
-// export default defineConfig({
-//   plugins: [react(), basicSsl()],
-//   server: {
-//     https: true,
-//     host: "localhost",
-//     port: 3000,
-//   },
-// });
