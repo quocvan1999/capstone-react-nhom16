cf60f560af99146cbe0e41951d0f93858f44ace4
Quocvan <lyquocvan1999@gmail.com>
2024-09-12 21:22:01 +0700

    update

--- src/components/favouriteProFile/FavouriteProFile.jsx
+++ src/components/favouriteProFile/FavouriteProFile.jsx
@@ -1,13 +1,11 @@
 import { useEffect } from "react";
 import useCheckLogin from "../../customHook/useCheckLogin/useCheckLogin";
-import { useDispatch, useSelector } from "react-redux";
-import { getProductFavoriteApiAsync } from "../../apis/product/getProductfavorite/GetProductFavorite.api";
+import { useSelector } from "react-redux";
 import Card from "../card/Card";
 import { Empty } from "antd";
 import useLoadingData from "../../customHook/useLoadingData/useLoadingData";
 
 const FavouriteProFile = () => {
-  const dispatch = useDispatch();
   const { handleLoadingData } = useLoadingData();
   const { isLogin } = useCheckLogin();
   const { productFavorite } = useSelector((state) => state.productReducer);
